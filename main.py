4cf8eae25517278e129d5bd3137c15977ee81d0a
Scip5 <justinscipio@gmail.com>
2017-06-20 11:28:49 -0700

    here is the code I made to create validation for web-caesar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from caesar import rotate_string
+from caesar import rotate_string 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -7,8 +7,8 @@ app.config['DEBUG'] = True
 form = """
 
 <!doctype html>
-<html>
-    <head>
+    <html>
+        <head>
             <style>
                 
                 form {{
@@ -20,39 +20,86 @@ form = """
                     font: 16px sans-serif;
                     border-radius: 10px;
                 }}
+
                 textarea {{
                     margin: 10px 0;
                     width: 540px;
                     height: 120px;
                 }}
+
+                .error {{
+                    color: red;
+                }}
+
             </style>
         </head>
         <body>
             <form method="POST">
+                
                 <label>Rotate by: 
-                    <input type="text" name="rot" value="0" />
+                    <input type="text" name="rot" value='{rotate}' />
                 </label>
-                <p>
-                </p>
-                <textarea name="text">{0}</textarea>
+
+                <p class="error">{input_error}</p>
+
+                <textarea name="text">{text}</textarea>
+
                 <br>
+
                 <input type="submit" />            
             </form>
-        </body>
-          
-</html>
+        </body>          
+    </html>
 """
-@app.route("/", methods=['POST'])
-def encrypt():
-    num = request.form['rot']
-    realNum = int(num)
-    string = request.form['text']
-    newString = rotate_string(string, realNum)
-    return form.format(newString) 
 
 @app.route("/")
 def index():
-    return form.format("")
+    return form.format(rotate='', input_error='' , text='')
+
+def is_integer(num):
+
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
+
+def number_alpha(num):
+    
+    num = int(num)
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    alpha_char = alphabet[num]
+    return alpha_char
+    
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot = request.form['rot']
+    text = request.form['text']
+    input_error = ""
+    numbers = "0123456789"
+    newString = ""
+    finalString = ""
+    
+
+    if not is_integer(rot):
+        input_error = "Input must be an integer"
+        rot = ""
+    else:
+        rot = int(rot)
+        newString = rotate_string(text, rot)
+
+    for char in newString:
+        if char in numbers:
+            letter = number_alpha(char)            
+            finalString = finalString + letter
+
+        else:
+            finalString = finalString + char
+
+    if not input_error:
+        return form.format(text=finalString) 
+    else:
+        return form.format(rotate=rotate, input_error=input_error, text=text)
 
 app.run()
 
